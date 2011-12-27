872c343d1f03c2f7e4bce3832f6a46267b8d35ea
Scott Walker <scott-walker@inbox.com>
2011-12-27 15:57:39 -0800

    Added comments

--- Python/3.0/src/MJ Puzzles/MagicCube/magiccube.py
+++ Python/3.0/src/MJ Puzzles/MagicCube/magiccube.py
@@ -1,12 +1,17 @@
+# Magic Cube
+#
+# This was a work in progress to help figure out a magic cube that possible used
+# 42 as the magic number. I was trying to see what combinations of three numbers
+# would add to this.
+
 combos = []
 
-if __name__ == '__main__':
-    for i in range (1, 28):
-        for j in range (1, 28):
-            if (i != j):
-                for k in range (1, 28):
-                    if (i != k) and (j != k) and (i+j+k == 42):
-                        combos.append((i,j,k))
+for i in range (1, 28):
+    for j in range (1, 28):
+        if (i != j):
+            for k in range (1, 28):
+                if (i != k) and (j != k) and (i + j + k == 42):
+                    combos.append((i, j, k))
 
 for combo in combos:
     print(combo)
